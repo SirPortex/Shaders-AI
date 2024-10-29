03649f3efd3ba862ffb2b2e95f457af3467addb5
Pavel.K144 <portex144@gmail.com>
2024-10-29 19:29:25 +0100

    Patrol01

--- Assets/Scripts/AI/PatrolState.cs
+++ Assets/Scripts/AI/PatrolState.cs
@@ -1,18 +1,24 @@
+using System.Buffers;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class PatrolState : MonoBehaviour
+[CreateAssetMenu(fileName = "PatrolState (S)", menuName = "ScriptableObject/States/PatrolState")] //IMPORTANTE
+
+public class PatrolState : State
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector3 guardPoint01, guardPoint02;
 
-    // Update is called once per frame
-    void Update()
+
+    public override State Run(GameObject owner)
     {
+
+        State nextState = CheckActions(owner);
+        NavMeshAgent navMeshAgent = owner.GetComponent<NavMeshAgent>(); // al herederar de ScriptableObject hay que poner owner para poder heredar el componente de NavMeshAgent
+
+        navMeshAgent.SetDestination(guardPoint01);
         
+        return nextState;
     }
 }
