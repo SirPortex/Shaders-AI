1e8493dd96228023e1db10cf46495cc1fcd19b61
SirPortex <portex144@gmail.com>
2024-10-30 09:43:22 +0100

    FunctionalPatrolState

--- Assets/Scripts/AI/PatrolState.cs
+++ Assets/Scripts/AI/PatrolState.cs
@@ -8,16 +8,41 @@ using UnityEngine.AI;
 
 public class PatrolState : State
 {
-    public Vector3 guardPoint01, guardPoint02;
+    public Vector3[] arrayPoints = new Vector3[1];
 
+    public float maxTime;
+
+    private float currenttime = 0;
+
+    private int currentIndex = 0;
 
     public override State Run(GameObject owner)
     {
+        
 
         State nextState = CheckActions(owner);
         NavMeshAgent navMeshAgent = owner.GetComponent<NavMeshAgent>(); // al herederar de ScriptableObject hay que poner owner para poder heredar el componente de NavMeshAgent
 
-        navMeshAgent.SetDestination(guardPoint01);
+        navMeshAgent.SetDestination(arrayPoints[currentIndex]);
+        
+        if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+        {
+            currenttime += Time.deltaTime;
+
+            if (currenttime >= maxTime)
+            {
+                currenttime = 0;
+                currentIndex++;
+
+                if (currentIndex >= arrayPoints.Length)
+                {
+                    currentIndex = 0;
+                }
+                
+            }
+        }
+        //remainingDistance <= stoppingDistance o Mathf.approx 
+ 
         
         return nextState;
     }
