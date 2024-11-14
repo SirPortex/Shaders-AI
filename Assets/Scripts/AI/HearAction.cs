3b95e7f7726cc2e7943b4e7b749fb9abbc30498a
Pavel.K144 <portex144@gmail.com>
2024-11-15 00:09:53 +0100

    Wander

---
+++ Assets/Scripts/AI/HearAction.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HearAction (A)", menuName = "ScriptableObject/Actions/HearAction")] //IMPORTANTE
+
+
+public class HearAction : Action
+{
+    public float radius = 20f;
+    public override bool Check(GameObject owner)
+    {
+        RaycastHit[] hits = Physics.SphereCastAll(owner.transform.position, radius, Vector3.up); // Casteamos una esfera alrededor del enemigo
+
+        GameObject target = owner.GetComponent<TargetReference>().target; // accedemos al target ,al igual que en el chase.
+
+        foreach (RaycastHit hit in hits) //Recorremos el array entero de los colliders y si uno de ellos es el target devuelve true
+        {
+            if (hit.collider.gameObject == target)
+            {
+                //le hemos escuchado / oler
+                return true;
+            }
+        }
+        return false; // no lo escucho
+
+    }
+
+    public override void DrawGizmos(GameObject owner)
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(owner.transform.position, radius);
+
+    }
+
+}
---
+++ Assets/Scripts/AI/WanderState.cs
@@ -0,0 +1,53 @@
+using System.Buffers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[CreateAssetMenu(fileName = "WanderState (S)", menuName = "ScriptableObject/States/WanderState")] //IMPORTANTE
+
+
+public class WanderState : State
+{
+
+    public float range;
+
+    public Vector3 centrePoint;
+
+
+    public override State Run(GameObject owner)
+    {
+        State nextState = CheckActions(owner);
+        NavMeshAgent navMeshAgent = owner.GetComponent<NavMeshAgent>();
+
+        if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+        {
+
+            Vector3 point;
+
+            if (RandomPoint(centrePoint, range, out point))
+            {
+                Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f);
+                navMeshAgent.SetDestination(point);
+            }
+
+        }
+
+        bool RandomPoint(Vector3 center, float range, out Vector3 result)
+        {
+            Vector3 randomPoint = center + Random.insideUnitSphere * range; //random point in a sphere 
+            NavMeshHit hit;
+
+            if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+            {
+                result = hit.position;
+                return true;
+            }
+
+            result = Vector3.zero;
+            return false;
+        }
+
+        return nextState;
+    }
+}
